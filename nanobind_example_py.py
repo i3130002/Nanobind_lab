1bb2406d90e7b860ebcd86df63694319f2dad5fe
i3130002 <i3130002@gmail.com>
2024-03-20 21:32:30 +0400

    Adding our python codes to run the nanobind_example package

--- nanobind_example_py.py
+++ nanobind_example_py.py
@@ -1,3 +1,30 @@
 import nanobind_example
-print(nanobind_example.add(1, 2))
+from icecream import ic
 
+# ic(nanobind_example.add(1, 2))
+# ic(nanobind_example.mul(1, 2))
+# ic(nanobind_example.mul2(1, 2))
+# ic(nanobind_example.mul3(1, 2))
+
+geekyOdyssey = nanobind_example.GeekyOdyssey()
+# ic(geekyOdyssey.print())
+# ic(geekyOdyssey.name)
+# geekyOdyssey.name = 4
+# ic(geekyOdyssey.name)
+
+
+# ic(nanobind_example.GeekyOdyssey.instances)
+# nanobind_example.GeekyOdyssey()
+# ic(nanobind_example.GeekyOdyssey.instances)
+
+# ic(nanobind_example.GeekyOdyssey.sprint())
+
+# ic(geekyOdyssey.get_vector())
+# ic(geekyOdyssey.print_vector([1,2,3,4]))
+
+
+# shared_int = geekyOdyssey.get_shared_int()
+# ic(shared_int)
+
+# geekyOdyssey.set_shared_int(10)
+# ic(shared_int)
